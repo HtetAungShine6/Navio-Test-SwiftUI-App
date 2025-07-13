469a8e34958104e2263fc22090cb6949fb93958f
HtetAungShine6 <htetaungshine211299@gmail.com>
2025-07-13 21:50:49 +0700

    Initial Commit

---
+++ Testing For Navio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Testing For Navio
+//
+//  Created by Htet Aung Shine on 13/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Testing For Navio/Testing_For_NavioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Testing_For_NavioApp.swift
+//  Testing For Navio
+//
+//  Created by Htet Aung Shine on 13/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Testing_For_NavioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
